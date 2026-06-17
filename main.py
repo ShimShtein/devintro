b3082ce6f9abb2c99c9d92ebb4686bbc396055de
Shimon Shtein <sshtein@redhat.com>
2026-06-17 10:33:45 +0300

    Add initial game code

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-print("hello world")
+import random
 
+number = random.randint(1, 10)
+print("The random number is", number)
+
+for attempt in range(5):
+    guessed_num = int(input("Try to guess the number:"))
+    if guessed_num == number:
+        print("You WIN")
+        break
+    print(f"Sorry, that is not the number, try again (attempt {attempt})")
